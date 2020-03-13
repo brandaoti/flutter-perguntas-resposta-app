eb7fa6848f2e548c21e0b07809ee8187f5348715
brandaoti <dannnbrandao@gmail.com>
2020-03-13 00:42:26 -0300

    Atualizado estutura pela class Perguntas

---
+++ lib/perguntas.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:flutter/material.dart';
+
+class PerguntasApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // implement build Scaffold
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Perguntas & Resposta'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
