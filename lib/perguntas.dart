98cd0d50841ffe8e51e7e32872c609e2437bd1ff
brandaoti <dannnbrandao@gmail.com>
2020-03-13 00:47:30 -0300

    Implementado Um text com possiveis perguntas e 'btns'

--- lib/perguntas.dart
+++ lib/perguntas.dart
@@ -1,8 +1,6 @@
-
-
 import 'package:flutter/material.dart';
 
-class PerguntasApp extends StatelessWidget{
+class PerguntasApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // implement build Scaffold
@@ -11,6 +9,17 @@ class PerguntasApp extends StatelessWidget{
         title: Text('Perguntas & Resposta'),
         centerTitle: true,
       ),
+      body: Column(
+        children: <Widget>[
+          Text(
+            'Qual sua cor favorita?',
+            style: TextStyle(fontSize: 24),
+          ),
+          RaisedButton(child: Text('Resposta #1'), onPressed: null),
+          RaisedButton(child: Text('Resposta #2'), onPressed: null),
+          RaisedButton(child: Text('Resposta #4'), onPressed: null),
+        ]
+      )
     );
   }
 }
