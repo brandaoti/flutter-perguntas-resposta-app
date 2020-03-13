ec52c53762f5f03c19600b36f6d16143bcc98f2d
brandaoti <dannnbrandao@gmail.com>
2020-03-13 01:01:49 -0300

    Att

--- lib/perguntas.dart
+++ lib/perguntas.dart
@@ -11,7 +11,7 @@ class PerguntasApp extends StatelessWidget {
 
   void _responder() {
     _perguntaSelecionada++;
-    print(_perguntaSelecionada);
+    //print(_perguntaSelecionada);
   }
 
   @override
